bef33af9fb502947051d8bb78b14d4e2459b9a03
sunliuchao <sunliuchao>
2021-01-11 22:07:26 +0800

    重刷树

--- code/98.验证二叉搜索树.js
+++ code/98.验证二叉搜索树.js
@@ -20,7 +20,6 @@ var isValidBST = function (root) {
   // 二叉搜索树（BST）的基本规则是
   // 左子树的所有节点的值都要小于根节点,子树同理
   // 右子树的所有节点的值都要大于根节点,子树同理
-
   // 递归
   // 本题的逻辑改为 节点的值在两个边界之间为真
   // const valid = function (node, min, max) {
@@ -35,27 +34,40 @@ var isValidBST = function (root) {
   //   return valid(node.left, min, node.val) && valid(node.right, node.val, max);
   // };
   // return valid(root, -Infinity, Infinity);
-
   // 中序遍历 左 根 右
   // 二叉搜索树的中序遍历是升序的
-  let stack = [];
-  let small = -Infinity;
-  while (stack.length || root) {
-    // 保存根节点然后找左子节点
-    while (root) {
-      stack.push(root);
-      root = root.left;
+  // let stack = [];
+  // let small = -Infinity;
+  // while (stack.length || root) {
+  //   // 保存根节点然后找左子节点
+  //   while (root) {
+  //     stack.push(root);
+  //     root = root.left;
+  //   }
+  //   // 左子节点为空时，取出根
+  //   root = stack.pop();
+  //   // 判断根节点的值是否满足
+  //   if (root.val <= small) {
+  //     return false;
+  //   }
+  //   small = root.val;
+  //   // 查看当前节点的右节点
+  //   root = root.right;
+  // }
+  // return true;
+
+  // 二叉搜索树定义： 当前节点的值大于所有左节点值，小于所有右节点值
+  function valid(node, lower, upper) {
+    if (!node) {
+      return true;
     }
-    // 左子节点为空时，取出根
-    root = stack.pop();
-    // 判断根节点的值是否满足
-    if (root.val <= small) {
+    if (node.val <= lower || node.val >= upper) {
       return false;
     }
-    small = root.val;
-    // 查看当前节点的右节点
-    root = root.right;
+
+    return valid(node.left, lower, node.val) && valid(node.right, node.val, upper);
   }
-  return true;
+
+  return valid(root, -Infinity, Infinity);
 };
 // @lc code=end
